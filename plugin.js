68197ec7f5e0db5abd48aaffd4fd5a3fc6b3a2fd
Peter Leonov <gojpeg@gmail.com>
2018-03-04 18:22:53 +0100

    a bit of DI

--- plugin.js
+++ plugin.js
@@ -1,58 +1,58 @@
 const push = (a, b) => a.push.apply(a, b)
 
-const getLocalSpecifierIdentifiers = (t, path) => {
-    const identifiers = []
-    const getLocalIdentifiers = path => {
-        const local = path.node.local
-        if (t.isIdentifier(local)) identifiers.push(local)
-    }
-    path.traverse({
-        ImportDefaultSpecifier: getLocalIdentifiers,
-        ImportSpecifier: getLocalIdentifiers,
-        ImportNamespaceSpecifier: getLocalIdentifiers
-    })
-    return identifiers
-}
-
-const getAndRemoveImportedIdentifiers = (t, path) => {
-    const identifiers = []
-    path.traverse({
-        ImportDeclaration(path) {
-            push(identifiers, getLocalSpecifierIdentifiers(t, path))
-            path.remove()
-        }
-    })
-    return identifiers
-}
-
-const removeExports = (t, path) => {
-    const identifiers = []
-    const remove = path => path.remove()
-    path.traverse({
-        ExportDefaultDeclaration: remove,
-        ExportNamedDeclaration: remove,
-        ExportAllDeclaration: remove
-    })
-    return identifiers
-}
-
-const identifiersToObjectProperties = (t, identifiers) =>
-    identifiers.map(identifier =>
-        t.objectProperty(identifier, identifier, undefined, true)
-    )
-
 export default function({ types: t }) {
+    const getLocalSpecifierIdentifiers = path => {
+        const identifiers = []
+        const getLocalIdentifiers = path => {
+            const local = path.node.local
+            if (t.isIdentifier(local)) identifiers.push(local)
+        }
+        path.traverse({
+            ImportDefaultSpecifier: getLocalIdentifiers,
+            ImportSpecifier: getLocalIdentifiers,
+            ImportNamespaceSpecifier: getLocalIdentifiers
+        })
+        return identifiers
+    }
+
+    const getAndRemoveImportedIdentifiers = path => {
+        const identifiers = []
+        path.traverse({
+            ImportDeclaration(path) {
+                push(identifiers, getLocalSpecifierIdentifiers(path))
+                path.remove()
+            }
+        })
+        return identifiers
+    }
+
+    const removeExports = path => {
+        const identifiers = []
+        const remove = path => path.remove()
+        path.traverse({
+            ExportDefaultDeclaration: remove,
+            ExportNamedDeclaration: remove,
+            ExportAllDeclaration: remove
+        })
+        return identifiers
+    }
+
+    const identifiersToObjectProperties = identifiers =>
+        identifiers.map(identifier =>
+            t.objectProperty(identifier, identifier, undefined, true)
+        )
+
     return {
         visitor: {
             Program: function(path) {
-                const importIds = getAndRemoveImportedIdentifiers(t, path)
-                removeExports(t, path)
+                const importIds = getAndRemoveImportedIdentifiers(path)
+                removeExports(path)
                 path.node.body = [
                     t.functionDeclaration(
                         t.identifier("wrapper"),
                         [
                             t.objectPattern(
-                                identifiersToObjectProperties(t, importIds)
+                                identifiersToObjectProperties(importIds)
                             )
                         ],
                         t.blockStatement(path.node.body)
