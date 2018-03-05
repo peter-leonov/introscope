da2f0e1d63a1930f5d872a31fa7b9ed8ccc51f2e
Peter Leonov <gojpeg@gmail.com>
2018-03-05 17:01:07 +0100

    save notes

--- src/foo.js
+++ src/foo.js
@@ -1,1 +1,30 @@
-export const foo = 4
+import x from "x"
+
+// the value changes during namespace evaluation
+let x = 0
+x++
+
+let val
+let testee = () => val
+val = 7
+
+const recur = () => recur
+
+// const getData = _scope.getData || (() => [x, 2, 3])
+// const sum = (acc, x) = acc + x
+// const sumData = (_scope) => {
+//     return (({sum}) => getData.reduce(sum))({getData, sum, sumData, ..._scope})
+// }
+
+// and then: closure(testPlan({sum: undefined}))(fsdfsdfadfasf)
+
+let allTheScope
+
+const sumData = userScope => {
+    return (({ a, b, c }) => getData.reduce(sum))({
+        ...allTheScope,
+        ...userScope
+    })
+}
+
+allTheScope = { a, b, c }
