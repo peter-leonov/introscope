83ca299775065206598850047e6ebbde6df52fca
Peter Leonov <gojpeg@gmail.com>
2018-03-06 19:04:29 +0100

    close to final design

--- src/foo.js
+++ src/foo.js
@@ -1,4 +1,4 @@
-import x from "x"
+// @introscope
 
 // the value changes during namespace evaluation
 let x = 0
@@ -10,21 +10,27 @@ val = 7
 
 const recur = () => recur
 
-// const getData = _scope.getData || (() => [x, 2, 3])
-// const sum = (acc, x) = acc + x
-// const sumData = (_scope) => {
-//     return (({sum}) => getData.reduce(sum))({getData, sum, sumData, ..._scope})
-// }
-
-// and then: closure(testPlan({sum: undefined}))(fsdfsdfadfasf)
-
-let allTheScope
-
-const sumData = userScope => {
-    return (({ a, b, c }) => getData.reduce(sum))({
-        ...allTheScope,
-        ...userScope
+const getData = _scope.getData || (() => [x, 2, 3])
+const sum = (acc, x) => acc + x
+const sumData = _scope => {
+    return (({ sum }) => getData.reduce(sum))({
+        getData,
+        sum,
+        sumData,
+        ..._scope
     })
 }
 
-allTheScope = { a, b, c }
+var allTheScope = { a, b, c }
+
+import introscope from 'introscope'
+//const introscope = <V>(v: V) => (scope: {}): V => v
+
+export var sumDataIntroscope = introscope(getData)
+export var sumDataIntroscope = userScope =>
+    (({ a, b, c }) => getData.reduce(sum))({
+        ...allTheScope,
+        ...userScope
+    })
+
+// and then: closure(testPlan({sum: undefined}))(fsdfsdfadfasf)
