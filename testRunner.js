858900345af193283cef98c98d730ac8af61307c
Peter Leonov <gojpeg@gmail.com>
2018-03-16 23:26:06 +0100

    document the horrible hack

--- testRunner.js
+++ testRunner.js
@@ -1,3 +1,18 @@
+/**
+ * This horrible hack does two things:
+ * 1. enables babel plugin via global variable to transpile
+ * 2. prevents Jest from caching the introscope transpiled code
+ *
+ * Why not just transpile the file content manually in a test code?
+ * Because it breaks a lot of things:
+ * 1. Jest will not add the introscoped file to watch mode
+ * 2. Jest automatically instruments code for coverage report
+ * 3. Jest can be configured to look for modules in non standard way
+ * 4. Jest can be configured with additional Babel plugins
+ * 5. Flow will not know where to get types from
+ * 6. Jump to file will not work in editors
+ */
+
 const Runtime = require('jest-runtime');
 
 const shouldInstrument = require('jest-runtime/build/should_instrument')
@@ -34,7 +49,7 @@ function transform(filename, options, fileSource) {
     // jest will not read from the cache
     this._config.cache = false;
     const original_getCacheKey = this._getCacheKey;
-    // jest will not write to the cache
+    // jest will not write to the normal cache
     this._getCacheKey = () => 'introscope-anti-cache';
     const transformed = this._transformAndBuildScript(
         filename,
