94241845c00638d89a00b8317df66c9acba28af1
Peter Leonov <gojpeg@gmail.com>
2018-05-08 23:21:51 +0200

    add more variadic values

--- shooter/effectsLogSnapshotSerializer.spec.js
+++ shooter/effectsLogSnapshotSerializer.spec.js
@@ -9,9 +9,14 @@ describe('spySnapshotSerializer', () => {
     const log = newShooterLog();
     log.push(
         ['call', 'function1', [1, 2, 3]],
+        ['call', 'function1', []],
+        ['call', 'function1', [null]],
         ['apply', 'function2', { a: 1 }, [1, 2, 3]],
+        ['apply', 'function2', null, []],
+        ['apply', 'function2', [1, 2, 3], ['str']],
         ['get', 'object1', 'property1'],
         ['set', 'object2', 'property2', [1, 2, 3]],
+        ['set', 'object2', 'property2', Symbol('foo')],
     );
 
     it('naked log', () => {
