3a7b3854ee9dd93d48d446dd5d77e22d84633b2e
Peter Leonov <gojpeg@gmail.com>
2018-09-19 22:12:55 +0200

    fix name

--- demo/src/todo.js
+++ demo/src/todo.js
@@ -6,14 +6,14 @@ const areFieldsValid = form => form.title !== '';
 const selectFormFields = state => state.fields;
 
 export const updateTodo = async (state, id) => {
-    trackEvent('todo.save', id);
+    trackEvent('todo.update', id);
     showLoadingSpinner();
     const fields = selectFormFields(state);
     if (areFieldsValid(fields)) {
         await post(`/todo/${id}`, fields);
-        trackEvent('todo.save.success', id);
+        trackEvent('todo.update.success', id);
     } else {
-        trackEvent('todo.save.failure', id);
+        trackEvent('todo.update.failure', id);
         showErrorMessage('BAD_FORM');
     }
     hideLoadingSpinner();
