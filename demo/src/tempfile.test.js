6eebf246adaa4701c1e2740838e9bcd140b0a92c
Petr Leonov <petr.leonov@zalando.de>
2018-09-20 11:44:19 +0200

    first show broken tests :)

--- demo/src/tempfile.test.js
+++ demo/src/tempfile.test.js
@@ -5,17 +5,17 @@ import { getRecorder } from './recorder';
 const logger = effectsLogger(introscope);
 
 test('generates a kinda uniq name', async () => {
-    const recorder = getRecorder('tempfile');
+    // const recorder = getRecorder('tempfile');
 
     const { scope, effects } = logger(
         {
             now: SPY,
             rand: SPY,
         },
-        { recorder },
+        // { recorder },
     );
 
     expect(scope.tempfile()).toMatchSnapshot();
 
-    recorder.save();
+    // recorder.save();
 });
